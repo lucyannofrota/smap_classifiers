382b173bba624c62385d9202adcde23982d25402
Lucyanno Frota <lucyannofrota@gmail.com>
2023-03-27 14:42:05 +0100

    opencv tests

--- launch/yolov5_launch.py
+++ launch/yolov5_launch.py
@@ -5,97 +5,50 @@ import launch
 from launch import LaunchDescription
 import launch_ros.actions
 from launch.actions import (IncludeLaunchDescription)
+from launch.actions import DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_context import LaunchContext
+from launch.substitutions import TextSubstitution
+from launch.substitutions import LaunchConfiguration
 
 # import os
 import yaml
 #from yaml import load
 from yaml import CLoader as Loader
-from smap.
+#from smap_classification_wrapper.classification_wrapper import main
+#from smap_perception_yolo_v5.yolo_v5_node import yolo_v5
+import launch
+import launch_ros.actions
+
 
 def generate_launch_description():
 
-    default_config=PathJoinSubstitution([FindPackageShare('smap_core'),'config/classifiers_config.yaml']).perform(LaunchContext())
+    model_launch_arg = DeclareLaunchArgument(
+        "model", default_value=TextSubstitution(
+        text=PathJoinSubstitution([
+            FindPackageShare('smap_perception_yolo_v5'),
+            'yolov5/yolov5s.torchscript'
+            ]).perform(LaunchContext())
+        ),
+        description='Model path [*.torchscript]'
+    )
 
-    # Installing Classifiers
-    config_path = ""
-    #classifiers.append({
-	#    "import_string": "from smap_classifiers.classifier_1 import classifier_1",
-	#    "class_name": "classifier_1",
-	#    "args": "\'yolov1\'"
-	#})
-    #classifiers.append({
-	#    "import_string": "from smap_classifiers.classifier_2 import classifier_2",
-	#    "class_name": "classifier_2",
-	#    "args": "\'yolov2\'"
-	#})
-
-    ofile = True
-    pfile=config_path
-    while True:
-        try:
-            with open(pfile,'r') as file: 
-                configs=yaml.load(file)
-        except OSError:
-            if(ofile):
-                ofile = False
-                print("Missing input arguments.")
-                print("The classifiers shoud be define using a launch file. Example: https://github.com/lucyannofrota/smap_core/blob/master/launch/importer_example_launch.py")
-                print("The default config file will be used instead!")
-                pfile=default_config
-                continue
-            else:
-                return 1
-        break
-    
-    for cls1 in configs['classifiers']:
-        try:
-            load_pkg_configs=cls1['external_pkg']
-        except:
-            load_pkg_configs={'pkg_name': 'smap_classifiers'}
-        
-        try:
-            pkg_path=PathJoinSubstitution([FindPackageShare(load_pkg_configs['pkg_name']),'config/']).perform(LaunchContext())
-            print(pkg_path)
-            # List files ls
-            # Try to found the correct classifier.yaml
-            # Open the file and read the configs
-            with open(
-                PathJoinSubstitution([FindPackageShare(load_pkg_configs['pkg_name']),'config/']).perform(LaunchContext()),
-                'r'
-            ) as cls1_file:
-                configs_cls2=yaml.load(cls1_file)
-            dfile=False
-            for cls2 in configs_cls2:
-                if cls2['name'] == cls1['cname']:
-                    dfile=True
-                    break
-            print("FOUND")
-        except:
-            print("Fail to load: {}".format(load_pkg_configs['name']))
-            pass
-
-
-
-    return
-
-    if cimporter(config_path,default_config):
-        print("Fail to load config file!")
-        return
-    #cimporter(classifiers)
-    # Node Setup
-
-    classification_node = launch_ros.actions.Node(
-            package='smap_core',
-            executable='classification_node.py',
-            output='screen'
+    yolo_v5_node = launch_ros.actions.Node(
+            package='smap_perception_yolo_v5',
+            executable='yolo_v5_node.py',
+            parameters=[{
+                "model": LaunchConfiguration('model')
+            }],
+            output='screen',
+            arguments=[('__log_level:=debug')],
+            emulate_tty=True
     )
 
     return LaunchDescription([
-        classification_node
+        model_launch_arg,
+        yolo_v5_node
     ])
 
 if __name__ == '__main__':
--- smap-perception-yolo-v5/yolo_v5_node.py
+++ smap-perception-yolo-v5/yolo_v5_node.py
@@ -6,15 +6,20 @@ from std_msgs.msg import String
 from smap_classification_wrapper.classification_wrapper import classification_wrapper,main
 from smap_interfaces.msg import SmapData, SmapPrediction
 
-import cv2
-
+#import cv2
+#from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 
 import torch
 import json
 
+# YOLO_V5 functionalities adapted from https://github.com/ultralytics/yolov5/blob/master/models/common.py
 
+import time
+import torchvision
+import numpy as np
+import math
 
-import os
+from smap_perception_yolo_v5.yolov5.detect import run
 
 class yolo_v5(classification_wrapper):
 
@@ -25,56 +30,72 @@ class yolo_v5(classification_wrapper):
             detector_architecture='yolo_v5'
         )
 
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model_file = "../yolov5/yolov5s.torchscript"
+        self.device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model_file="/workspace/install/share/smap_perception_yolo_v5/yolov5/yolov5s.torchscript"
+        self.imgsz=(1, 3, 640, 640)
+
+        run()
+
+        #self.br = CvBridge()
         
-    def predict(self,data):
-        msg = SmapPrediction()
-        msg.detector_id = 5
-        self.publisher.publish(msg)
+    def predict(self,msg):
+        #msg.stamped_pose
+        #msg.rgb_image
+        #msg.pointcloud
+
+        print('<Pred')
+        
+
+        #img=torch.from_numpy(img).to(self.model.device)
+        #img=img.float()
+        #img/=255
+        #if len(img.shape) == 3:
+        #    img = img[None]  # expand for batch dim
+        #pred = self.model(img)
+        #pred = non_max_suppression(
+        #    pred,
+        #    conf_thres=0.25,
+        #    iou_thres=0.54,
+        #    classes=None,
+        #    agnostic_nms=False,
+        #    max_det=1000
+        #)
+
+        #current_frame = self.br.imgmsg_to_cv2(msg.rgb_image)
+        print(msg.rgb_image.height)
+        print(msg.rgb_image.width)
+
+        #cv2.imshow("camera", current_frame)
+    
+        #cv2.waitKey(0)
+        
+
+        resp_msg = SmapPrediction()
+        resp_msg.module_id = 5
+        self.publisher.publish(resp_msg)
+        print('>Pred')
 
     def _load_model(self): # Return True when an error occurs
         if super()._load_model():  
             return True
-        print('model_file')
-        print(self.model_file)
         self.model_file = str(self.model_file)
         extra_files = {'config.txt': ''}  # model metadata
-        print('pre')
         self.model = torch.jit.load(self.model_file, _extra_files=extra_files, map_location=self.device)
-        print(self.model)
-        #model.half() if fp16 else model.float()
         if extra_files['config.txt']:  # load metadata dict
             d = json.loads(extra_files['config.txt'],
                             object_hook=lambda d: {int(k) if k.isdigit() else k: v
                                                     for k, v in d.items()})
             self.stride, self.classes = int(d['stride']), d['names']
 
-        self.get_logger().warning("yolo_load_model")
+        self.get_logger().info("{} loaded.".format(self.model_file))
         return False
-
-
-def _load_model(self): # Return True when an error occurs
-
-    self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    self.model_file = "yolov5/yolov5s.torchscript"
-    print('model_file')
-    print(self.model_file)
-    self.model_file = str(self.model_file)
     
-    extra_files = {'config.txt': ''}  # model metadata
-    print('pre')
-    self.model = torch.jit.load(self.model_file, _extra_files=extra_files, map_location=self.device)
-    print(self.model)
-    #model.half() if fp16 else model.float()
-    if extra_files['config.txt']:  # load metadata dict
-        d = json.loads(extra_files['config.txt'],
-                        object_hook=lambda d: {int(k) if k.isdigit() else k: v
-                                                for k, v in d.items()})
-        self.stride, self.classes = int(d['stride']), d['names']
+    def _model_warmup(self):
+        self.get_logger().info('Model warming up...')
+        img = torch.empty(*self.imgsz, dtype=torch.float, device=self.device)  # input
+        self.model.forward(img)
 
-    self.get_logger().warning("yolo_load_model")
-    return False
+
 
 if __name__ == '__main__':
 
